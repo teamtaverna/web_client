7f5482b72408079579f64bf2d07921ad80c81531
Rukayat Sadiq <fruqoyya@gmail.com>
2016-10-05 23:09:39 +0100

    (WIP) set up initial express server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+<<<<<<< 4fce8506d7bcce7ff1a9574fde01bc492bfbb8f3
 /* eslint-disable */
 import React from 'react';
 
@@ -10,6 +11,12 @@ const app = express();
 app.get('*', (req, res, next) => {
   res.status(200).send(`<!doctype html>\n${ReactDOM.renderToString(<HelloWorld />)}`);
 });
+=======
+const express = require('express');
+
+const app = express();
+
+>>>>>>> (WIP) set up initial express server
 app.listen(5000, (err) => {
   if (err) {
     console.error(err);
