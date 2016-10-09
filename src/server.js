f47e155d625406a3b2d5a80f066104c2ff9b7d94
Rukayat Sadiq <fruqoyya@gmail.com>
2016-10-09 01:54:33 +0100

    render react component from server side

--- src/server.js
+++ src/server.js
@@ -1,22 +1,14 @@
-<<<<<<< 4fce8506d7bcce7ff1a9574fde01bc492bfbb8f3
 /* eslint-disable */
 import React from 'react';
 
 import ReactDOM from 'react-dom/server';
 const express = require('express');
-
 import { HelloWorld } from './client';
 const app = express();
 
 app.get('*', (req, res, next) => {
   res.status(200).send(`<!doctype html>\n${ReactDOM.renderToString(<HelloWorld />)}`);
 });
-=======
-const express = require('express');
-
-const app = express();
-
->>>>>>> (WIP) set up initial express server
 app.listen(5000, (err) => {
   if (err) {
     console.error(err);
