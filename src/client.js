4ffd32b2601ca39ecca4a7feab772fcf3e72491d
Rukayat Sadiq <fruqoyya@gmail.com>
2016-10-09 01:47:13 +0100

    create test component for client side

---
+++ src/client.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export class HelloWorld extends Component {
+  render() {
+    return (
+            <p>Hello World!</p>
+        );
+  }
+}
+// ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
