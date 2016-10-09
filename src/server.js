e84aaaf3170176803513a0a23f461e18d0a03375
Rukayat Sadiq <fruqoyya@gmail.com>
2016-10-09 01:54:33 +0100

    render react component from server side

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
+/* eslint-disable */
+import React from 'react';
+
+import ReactDOM from 'react-dom/server';
 const express = require('express');
 
+import { HelloWorld } from './client';
 const app = express();
 
+app.get('*', (req, res, next) => {
+  res.status(200).send(`<!doctype html>\n${ReactDOM.renderToString(<HelloWorld />)}`);
+});
 app.listen(5000, (err) => {
   if (err) {
     console.error(err);
