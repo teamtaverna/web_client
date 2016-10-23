17ebac5a7509cb019ef501b6b028322baa46d103
andela-ashuaib <abiodun.shuaib@andela.com>
2016-10-23 13:19:33 +0100

    add new linter changes for es6 components, adds cssloader

--- webpack/common.config.js
+++ webpack/common.config.js
@@ -37,7 +37,6 @@ module.exports = {
       test: /\.less$/,
       loader: 'style!css!less'
     },
-
     // SASS
     {
       test: /\.scss$/,
@@ -45,9 +44,6 @@ module.exports = {
     }, 
     { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
       ,
-
-    // Needed for the css-loader if you're using bootstrap-webpack(https://github.com/bline/bootstrap-webpack)
-    // loads bootstrap's css.
     {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
     {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
     {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
