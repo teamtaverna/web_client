94e1d2b472fd8d0f414c4d771141e4128c0c6d89
Awili Uzo <uzo.awili@andela.com>
2016-11-24 14:27:00 +0100

    refactor layout to include the sidebar

--- static_build/js/script.js
+++ static_build/js/script.js
@@ -1,6 +1,6 @@
 const $ = window.$;
 
-$.ready(() => {
+$(() => {
   // initialize semantic ui accordion module:
   $('.ui.accordion').accordion({
     exclusive: false,
@@ -10,5 +10,18 @@ $.ready(() => {
   $('.serving.card .ui.rating').rating({
     maxRating: 5,
     interactive: false,
+  });
+
+  // initialize semantic ui sidebar:
+  $('.sidebar')
+    .sidebar({
+      context: $('.tvn.page > .main'),
+      scrollLock: true,
+    })
+    .sidebar('setting', 'transition', 'push');
+
+  // toggle the sidebar when you click a card
+  $('.serving.card').click(() => {
+    $('.sidebar').sidebar('toggle');
   });
 });
