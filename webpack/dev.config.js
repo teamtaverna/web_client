b174b6c02efbbd53bbebfc0f5b6818bfb7936c33
Abiodun Shuiab <abiodun@Abioduns-MacBook-Pro.local>
2016-12-09 20:37:49 +0100

    Adds pre-configured css that is supposedly suppose to be able to work with css-modules per each of the component

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,19 +1,17 @@
 /* eslint-disable */
 const webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./common.config.js');
 const IsomorphicPlugin = require('webpack-isomorphic-tools/plugin');
 const path = require('path')
 const webpackIsomorphicToolsPlugin = new IsomorphicPlugin(require('./isomorphic.config'));
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'source-map',
   plugins: [
-    new ExtractTextPlugin('[name].css'),
     webpackIsomorphicToolsPlugin.development()
   ],
-
+  debug: true,
   devServer: {
     historyApiFallback: true,
     stats: 'minimal',
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 var webpack = require('webpack');
 var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./common.config.js');
 const IsomorphicPlugin = require('webpack-isomorphic-tools/plugin');
 
@@ -11,19 +10,9 @@ const ENV = process.env.NODE_ENV;
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-source-map',
-  modules:{
-      loaders: [{
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'postcss-loader',
-          loader: 'css?modules&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true' // eslint-disable-line max-len
-        })
-      }]
-  },
   plugins: [
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
-    new ExtractTextPlugin('[name].[hash].css'),
     new webpack.DefinePlugin({
       'process.env': {
         'ENV': JSON.stringify(ENV)
