81df6eb698301adede92867a0b6336c1c80fef01
andela-akolawole <aladekolawole15@yahoo.com>
2017-02-19 21:18:46 +0100

    55_add_global_styles

--- src/client.jsx
+++ src/client.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './containers';
 
+require('./scss/style.scss');
+
 const mountNode = document.getElementById('content');
 render(<App />, mountNode, () => {
   console.log('client side now active');
